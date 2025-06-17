aca74187c90bc8dd2ccb2476b2f112de05189de5
IShix <49128115+IShix-g@users.noreply.github.com>
2025-06-17 15:34:47 +0900

    feat: Improve PersistentLifetimeScope with custom parent resolution and multi-object handling

--- Packages/PersistentLifetimeScope/RunTime/PersistentLifetimeScope.cs
+++ Packages/PersistentLifetimeScope/RunTime/PersistentLifetimeScope.cs
@@ -1,4 +1,7 @@
 #if ENABLE_VCONTAINER
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace VContainer.Unity.Extensions
 {
@@ -8,13 +11,13 @@ namespace VContainer.Unity.Extensions
     public abstract class PersistentLifetimeScope<T> : PersistentLifetimeScopeBase where T : PersistentLifetimeScope<T>
     {
         static T s_instance;
-        
+
         protected override void Awake()
         {
             if (s_instance == null)
             {
                 base.Awake();
-                s_instance = (T)this;
+                s_instance = (T) this;
                 DontDestroyOnLoad(gameObject);
             }
             else if (s_instance != this)
@@ -43,6 +46,41 @@ namespace VContainer.Unity.Extensions
         protected virtual void Reset()
         {
             parentReference = default;
+        }
+
+        protected override LifetimeScope FindParent()
+        {
+            var objs = FindObjects(parentReference.Type);
+            if (objs.Length > 1)
+            {
+                foreach (var obj in objs)
+                {
+                    var scope = (LifetimeScope) obj;
+                    if (scope.gameObject.scene.name == "DontDestroyOnLoad"
+                        && scope.Container != null)
+                    {
+                        return scope;
+                    }
+                }
+            }
+            {
+                if (objs.Length > 0
+                    && objs[0] is LifetimeScope scope
+                    && scope.Container != null)
+                {
+                    return scope;
+                }
+            }
+            return null;
+        }
+
+        static Object[] FindObjects(Type type)
+        {
+#if UNITY_2022_1_OR_NEWER
+            return FindObjectsByType(type, FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+#else
+            return FindObjectsOfType(type);
+#endif
         }
     }
 }
